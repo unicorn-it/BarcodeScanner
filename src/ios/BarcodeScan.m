f810cd50024c2ae9605edbd364dca1a33b70bf4d
south-pacific <south-pacific@foxmail.com>
2014-08-06 21:23:34 +0800

    IOS扫描修改，修改为使用IOS7自带方法，仅支持IOS7+
    Signed-off-by: south-pacific <south-pacific@foxmail.com>

---
+++ src/ios/BarcodeScan.m
@@ -0,0 +1,252 @@
+//
+//  BarcodeScan.m
+//  QiFei
+//
+//  Created by 雷彬 on 14-8-6.
+//
+//
+
+#import "BarcodeScan.h"
+
+@implementation BarcodeScan
+
+//--------------------------------------------------------------------------
+- (NSString*)isScanNotPossible {
+    NSString* result = nil;
+    
+    Class aClass = NSClassFromString(@"AVCaptureSession");
+    if (aClass == nil) {
+        return @"AVFoundation Framework not available";
+    }
+    
+    return result;
+}
+
+//--------------------------------------------------------------------------
+- (void)scan:(CDVInvokedUrlCommand*)command {
+    
+    NSString*       callback;
+    NSString*       capabilityError;
+
+    callback = command.callbackId;
+
+    capabilityError = [self isScanNotPossible];
+    if (capabilityError) {
+        [self returnError:capabilityError callback:callback];
+        return;
+    }
+    
+    RootViewController * rt = [[RootViewController alloc]initWithPlugin:self callback:callback];
+    [self.viewController presentViewController:rt animated:YES completion:^{
+        
+    }];
+    
+}
+
+//--------------------------------------------------------------------------
+- (void)encode:(CDVInvokedUrlCommand*)command {
+    [self returnError:@"encode function not supported" callback:command.callbackId];
+}
+
+//--------------------------------------------------------------------------
+- (void)returnSuccess:(NSString*)scannedText format:(NSString*)format cancelled:(BOOL)cancelled callback:(NSString*)callback {
+    NSNumber* cancelledNumber = [NSNumber numberWithInt:(cancelled?1:0)];
+    
+    NSMutableDictionary* resultDict = [[NSMutableDictionary alloc] init];
+    [resultDict setObject:scannedText     forKey:@"text"];
+    [resultDict setObject:format          forKey:@"format"];
+    [resultDict setObject:cancelledNumber forKey:@"cancelled"];
+    
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus: CDVCommandStatus_OK
+                               messageAsDictionary: resultDict
+                               ];
+    
+    NSString* js = [result toSuccessCallbackString:callback];
+    
+    [self writeJavascript:js];
+}
+
+//--------------------------------------------------------------------------
+- (void)returnError:(NSString*)message callback:(NSString*)callback {
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus: CDVCommandStatus_OK
+                               messageAsString: message
+                               ];
+    
+    NSString* js = [result toErrorCallbackString:callback];
+    
+    [self writeJavascript:js];
+}
+
+@end
+
+
+
+
+
+
+
+
+@implementation RootViewController
+
+@synthesize plugin               = _plugin;
+@synthesize callback             = _callback;
+
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//    if (self) {
+//        // Custom initialization
+//    }
+//    return self;
+//}
+
+- (id)initWithPlugin:(BarcodeScan *)plugin callback:(NSString*)callback
+{
+    self = [super initWithNibName:nil bundle:nil];
+    if (self) {
+        // Custom initialization
+        self.plugin = plugin;
+        self.callback = callback;
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor grayColor];
+	UIButton * scanButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [scanButton setTitle:@"取消" forState:UIControlStateNormal];
+    scanButton.frame = CGRectMake(100, 420, 120, 40);
+    [scanButton addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:scanButton];
+    
+    UILabel * labIntroudction= [[UILabel alloc] initWithFrame:CGRectMake(15, 40, 290, 50)];
+    labIntroudction.backgroundColor = [UIColor clearColor];
+    labIntroudction.numberOfLines=2;
+    labIntroudction.textColor=[UIColor whiteColor];
+    labIntroudction.text=@"将条形码或者二维码图像置于矩形方框内，离手机摄像头10CM左右。";
+    [self.view addSubview:labIntroudction];
+    
+    
+    UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(10, 100, 300, 300)];
+    imageView.image = [UIImage imageNamed:@"pick_bg"];
+    [self.view addSubview:imageView];
+    
+    upOrdown = NO;
+    num =0;
+    _line = [[UIImageView alloc] initWithFrame:CGRectMake(50, 110, 220, 2)];
+    _line.image = [UIImage imageNamed:@"line.png"];
+    [self.view addSubview:_line];
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:.02 target:self selector:@selector(animation1) userInfo:nil repeats:YES];
+}
+
+-(void)animation1
+{
+    if (upOrdown == NO) {
+        num ++;
+        _line.frame = CGRectMake(50, 110+2*num, 220, 2);
+        if (2*num == 280) {
+            upOrdown = YES;
+        }
+    }
+    else {
+        num --;
+        _line.frame = CGRectMake(50, 110+2*num, 220, 2);
+        if (num == 0) {
+            upOrdown = NO;
+        }
+    }
+    
+}
+
+-(void)backAction
+{
+    
+    [self dismissViewControllerAnimated:YES completion:^{
+        [timer invalidate];
+        [self.plugin returnSuccess:@"" format:@"" cancelled:TRUE callback:self.callback];
+    }];
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [self setupCamera];
+}
+
+- (void)setupCamera
+{
+    // Device
+    _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    // Input
+    _input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
+    
+    // Output
+    _output = [[AVCaptureMetadataOutput alloc]init];
+    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    
+    // Session
+    _session = [[AVCaptureSession alloc]init];
+    [_session setSessionPreset:AVCaptureSessionPresetHigh];
+    if ([_session canAddInput:self.input])
+    {
+        [_session addInput:self.input];
+    }
+    
+    if ([_session canAddOutput:self.output])
+    {
+        [_session addOutput:self.output];
+    }
+    
+    // 条码类型 AVMetadataObjectTypeQRCode
+    _output.metadataObjectTypes =@[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code,
+                                   AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code, AVMetadataObjectTypeCode39Mod43Code,
+                                   AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypePDF417Code,
+                                   AVMetadataObjectTypeAztecCode];
+    
+    // Preview
+    _preview =[AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    _preview.frame =CGRectMake(20,110,280,280);
+    [self.view.layer insertSublayer:self.preview atIndex:0];
+    
+    
+    
+    // Start
+    [_session startRunning];
+}
+#pragma mark AVCaptureMetadataOutputObjectsDelegate
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    
+    NSString *stringValue;
+    
+    if ([metadataObjects count] >0)
+    {
+        AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex:0];
+        stringValue = metadataObject.stringValue;
+    }
+    
+    [_session stopRunning];
+    [self dismissViewControllerAnimated:YES completion:^
+     {
+         [timer invalidate];
+         NSLog(@"%@",stringValue);
+         [self.plugin returnSuccess:stringValue format:@"" cancelled:FALSE callback:self.callback];
+     }];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    self.plugin = nil;
+    self.callback = nil;
+}
+
+@end
+
