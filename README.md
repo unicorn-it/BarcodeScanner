d66a01777cb063d1b0efda312e96e9dfbd400094
Clement <clement.delain@gmail.com>
2014-09-18 14:51:19 +0200

    FrenchBarcodeScanner

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+BarcodeScanner show Change
+==============
+In this Fork, just change the ios platform with four formats `EAN_13 EAN_8 CODE_39 QR_CODE` on the return object and rework the design for French app.
+
+[![](https://adowya.fr/img/barcode.png)]('Screen')
+
+Good luck
+
 BarcodeScanner
 ==============
 
--- src/ios/BarcodeScan.m
+++ src/ios/BarcodeScan.m
@@ -1,8 +1,7 @@
 //
 //  BarcodeScan.m
-//  QiFei
 //
-//  Created by 雷彬 on 14-8-6.
+//  Created by Adowya on 14-09-20.
 //
 //
 
@@ -27,9 +26,9 @@
     
     NSString*       callback;
     NSString*       capabilityError;
-
+    
     callback = command.callbackId;
-
+    
     capabilityError = [self isScanNotPossible];
     if (capabilityError) {
         [self returnError:capabilityError callback:callback];
@@ -116,22 +115,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor grayColor];
-	UIButton * scanButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [scanButton setTitle:@"取消" forState:UIControlStateNormal];
-    scanButton.frame = CGRectMake(100, 420, 120, 40);
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIButton * scanButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [scanButton setTitle:@"Annuler" forState:UIControlStateNormal];
+    scanButton.frame = CGRectMake(0, self.view.bounds.size.height - 40, self.view.bounds.size.width, 40);
+    scanButton.autoresizingMask = UIViewAutoresizingFlexibleTopMargin;
+    scanButton.backgroundColor = [UIColor colorWithWhite:0.95 alpha:0.65];
     [scanButton addTarget:self action:@selector(backAction) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:scanButton];
     
-    UILabel * labIntroudction= [[UILabel alloc] initWithFrame:CGRectMake(15, 40, 290, 50)];
+    
+    
+    UILabel * labIntroudction= [[UILabel alloc] initWithFrame:CGRectMake(50, 40, 290, 50)];
     labIntroudction.backgroundColor = [UIColor clearColor];
     labIntroudction.numberOfLines=2;
-    labIntroudction.textColor=[UIColor whiteColor];
-    labIntroudction.text=@"将条码/二维码置于框内";
+    labIntroudction.textColor=[UIColor blackColor];
+    labIntroudction.text=@"Veuillez scanner votre article";
     [self.view addSubview:labIntroudction];
     
     
-    UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(10, 100, 300, 300)];
+    UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectMake(30, 100, 260, 260)];
     imageView.image = [UIImage imageNamed:@"pick_bg"];
     [self.view addSubview:imageView];
     
@@ -141,21 +145,21 @@
     _line.image = [UIImage imageNamed:@"line.png"];
     [self.view addSubview:_line];
     
-    timer = [NSTimer scheduledTimerWithTimeInterval:.02 target:self selector:@selector(animation1) userInfo:nil repeats:YES];
+    timer = [NSTimer scheduledTimerWithTimeInterval:.008 target:self selector:@selector(animation1) userInfo:nil repeats:YES];
 }
 
 -(void)animation1
 {
     if (upOrdown == NO) {
         num ++;
-        _line.frame = CGRectMake(50, 110+2*num, 220, 2);
-        if (2*num == 280) {
+        _line.frame = CGRectMake(50, 130+2*num, 220, 2);
+        if (2*num == 200) {
             upOrdown = YES;
         }
     }
     else {
         num --;
-        _line.frame = CGRectMake(50, 110+2*num, 220, 2);
+        _line.frame = CGRectMake(50, 130+2*num, 220, 2);
         if (num == 0) {
             upOrdown = NO;
         }
@@ -196,6 +200,8 @@
     _output = [[AVCaptureMetadataOutput alloc]init];
     [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
     
+    
+    
     // Session
     _session = [[AVCaptureSession alloc]init];
     [_session setSessionPreset:AVCaptureSessionPresetHigh];
@@ -209,18 +215,44 @@
         [_session addOutput:self.output];
     }
     
-    // 条码类型 AVMetadataObjectTypeQRCode
+    // AVMetadataObjectTypeQRCode
     //_output.metadataObjectTypes =@[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code,
     //                               AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code, AVMetadataObjectTypeCode39Mod43Code,
     //                               AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypePDF417Code,
     //                               AVMetadataObjectTypeAztecCode];
-                                   
-    _output.metadataObjectTypes =@[AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code];
+    
+    _output.metadataObjectTypes =@[AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeQRCode];
+    
+    
+    CGRect bounds = self.view.bounds;
+    bounds = CGRectMake(0, 0, bounds.size.width, bounds.size.height);
+    
+    UIView* overlayView = [[UIView alloc] initWithFrame:bounds];
+    overlayView.autoresizesSubviews = YES;
+    overlayView.autoresizingMask    = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    overlayView.opaque              = NO;
+    
+    bounds = overlayView.bounds;
+    CGFloat rootViewHeight = CGRectGetHeight(bounds);
+    CGFloat rootViewWidth  = CGRectGetWidth(bounds);
+    CGFloat minAxis = MIN(rootViewHeight, rootViewWidth);
+    
+    if ([_preview isOrientationSupported]) {
+        [_preview setOrientation:AVCaptureVideoOrientationPortrait];
+    }
     
     // Preview
     _preview =[AVCaptureVideoPreviewLayer layerWithSession:self.session];
     _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
-    _preview.frame =CGRectMake(20,110,280,280);
+    //    _preview.frame =CGRectMake(20,110,280,280);
+    _preview.frame = self.view.bounds;
+    //    _preview.frame =CGRectMake(
+    //                               0.5 * (rootViewWidth  - minAxis),
+    //                               0.5 * (rootViewHeight - minAxis),
+    //                               minAxis,
+    //                               minAxis
+    //                               );
+    
     [self.view.layer insertSublayer:self.preview atIndex:0];
     
     
@@ -233,11 +265,28 @@
 {
     
     NSString *stringValue;
+    NSString *format = nil;
+    
     
     if ([metadataObjects count] >0)
     {
         AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex:0];
+        
+        if ([[metadataObject type] isEqualToString:AVMetadataObjectTypeEAN13Code]) {
+            format =[NSString stringWithFormat:@"EAN_13"];
+        }
+        else if ([[metadataObject type] isEqualToString:AVMetadataObjectTypeEAN8Code]) {
+            format = [NSString stringWithFormat:@"EAN_8"];
+        }
+        else if ([[metadataObject type] isEqualToString:AVMetadataObjectTypeCode39Code]) {
+            format = [NSString stringWithFormat:@"CODE_39"];
+        }
+        else if ([[metadataObject type] isEqualToString:AVMetadataObjectTypeQRCode]) {
+            format = [NSString stringWithFormat:@"QR_CODE"];
+        }
+        
         stringValue = metadataObject.stringValue;
+        
     }
     
     [_session stopRunning];
@@ -245,7 +294,8 @@
      {
          [timer invalidate];
          NSLog(@"%@",stringValue);
-         [self.plugin returnSuccess:stringValue format:@"" cancelled:FALSE callback:self.callback];
+         NSLog(@"%@",format);
+         [self.plugin returnSuccess:stringValue format:format cancelled:FALSE callback:self.callback];
      }];
 }
 
@@ -255,6 +305,16 @@
     // Dispose of any resources that can be recreated.
     self.plugin = nil;
     self.callback = nil;
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return UIInterfaceOrientationPortrait;
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
 }
 
 @end
